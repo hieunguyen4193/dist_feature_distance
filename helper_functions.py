1666dae04161040eaa3acf1921a9a45cf388ebf6
hieunguyen4193 <work@hieunguyen.de>
2024-09-06 02:40:16 +0200

    update

--- helper_functions.py
+++ helper_functions.py
@@ -39,23 +39,30 @@ def calculate_barycenter(A, n = 301, show_plot=False, M = None):
         plt.show()
     return bary_l2
 
-def calculate_ot_distance_to_ref(input_sample, bary_l2, inputdf, n = 301):
-    x = np.arange(n, dtype=np.float64)
-    M = ot.dist(x.reshape((n, 1)), x.reshape((n, 1)), 'euclidean')
-    M /= M.max() * 0.1
-    a = inputdf[input_sample].values
-    a = np.array(a)
-    b = bary_l2
-    d_emd = ot.emd2(a, b, M)  # direct computation of OT loss
-    return d_emd
-
-def ot_distance(a, b, n, M = None):
+def calculate_ot_distance_to_ref(input_sample, bary_l2, inputdf, n = 301, distance = "sinkhorn", M = None):
     x = np.arange(n, dtype=np.float64)
     if M is None:
         M = ot.dist(x.reshape((n, 1)), x.reshape((n, 1)), 'euclidean')
         M /= M.max() * 0.1
-    d_emd = ot.emd2(a, b, M)  # direct computation of OT loss
-    return d_emd
+    a = inputdf[input_sample].values
+    a = np.array(a)
+    b = bary_l2
+    if distance == "emd2":
+        d = ot.emd2(a, b, M)  # direct computation of OT loss
+    elif distance == "sinkhorn":
+        d = ot.sinkhorn2(a, b, M, 1e-1)
+    return d
+
+def ot_distance(a, b, n, M = None, distance = 'emd2'):
+    x = np.arange(n, dtype=np.float64)
+    if M is None:
+        M = ot.dist(x.reshape((n, 1)), x.reshape((n, 1)), 'euclidean')
+        M /= M.max() * 0.1
+    if distance == "emd2":
+        d = ot.emd2(a, b, M)  # direct computation of OT loss
+    elif distance == "sinkhorn":
+        d = ot.sinkhorn2(a, b, M, 1e-1)
+    return d
 
 def ot_distance_flen(a, b, n = 301):
     x = np.arange(n, dtype=np.float64)
